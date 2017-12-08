8c2f190d674f934f9a3dc6c8591ab641137500e4
leo <leonardo@inetpub.co.uk>
2017-12-08 17:38:55 +0000

    PCF twicks

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,10 @@ var config = {
   module : {
     loaders : [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: ['es2015', 'react'] }
       },
       {
         test: /\.css$/,
