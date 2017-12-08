19bb8c7d0a722c81245635c9a4ac03e6af444dab
Leonardo Silva <leo@Leonardos-MacBook-Pro.local>
2017-12-08 20:27:19 +0000

    Text issues fix

--- src/client/app/footer.jsx
+++ src/client/app/footer.jsx
@@ -4,7 +4,7 @@ class Footer extends React.Component {
 	render (){
 		return (
 			<footer>
-			  <p><a href="mailto:contact@inetpub.co.uk">&#9993;</a> contact@inetpub.co.uk</p>
+			  <p>© Inetpub. All rights reserved. Site designed by us, yes baby we can code! <a href="mailto:contact@inetpub.co.uk">&#9993;</a> contact@inetpub.co.uk</p>
 			</footer>
 			);
 	}
--- src/client/app/home.jsx
+++ src/client/app/home.jsx
@@ -5,7 +5,7 @@ class Home extends React.Component {
     return (
       <div>
          <h2 className="title">Passion for Technology</h2>
-         <p>This simple title really well describes our company. We are absolutely passionate about what we do in IT, we care about IT, we lost our sleep thinking about IT, we lost the time doing IT and in summary, we love IT. If you share the same feeling about your project, please click on Contact and lets chat about it.</p>
+         <p>This simple title really well describes our company. We are absolutely passionate about what we do in IT, we care about IT, we lost our sleep thinking about IT, we lost the time doing IT and in summary, we love IT. If you share the same feeling about your project, please click on Contact and let's chat about it.</p>
       </div>
     );
   }
