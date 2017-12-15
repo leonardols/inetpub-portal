124cef66b75e2a433b935618b5aecf24863b7a3d
Leonardo Silva <leo@Leonardos-MacBook-Pro.local>
2017-12-15 15:03:28 +0000

    Mobile version added arrows

--- src/client/app/mobile_menu.jsx
+++ src/client/app/mobile_menu.jsx
@@ -8,11 +8,11 @@ class MobileMenu extends React.Component{
       return (
         <nav>
           <ul className="mobile_header">
-            <li><NavLink exact to="/" activeClassName="mobile_active">Home</NavLink><i className="fa fa-chevron-circle-down"></i></li>
-            <li><NavLink to="/services" activeClassName="mobile_active">Services</NavLink><i className="fa fa-chevron-circle-down"></i></li>
-            <li><NavLink to="/about" activeClassName="mobile_active">About</NavLink><i className="fa fa-chevron-circle-down"></i></li>
-            <li><NavLink to="/contact" activeClassName="mobile_active">Contact</NavLink><i className="fa fa-chevron-circle-down"></i></li>
-            <li><NavLink to="/apps" activeClassName="mobile_active">Apps</NavLink><i className="fa fa-chevron-circle-down"></i></li>
+            <li><NavLink exact to="/" activeClassName="mobile_active">Home<i className="fa fa-chevron-circle-down"></i></NavLink></li>
+            <li><NavLink to="/services" activeClassName="mobile_active">Services<i className="fa fa-chevron-circle-down"></i></NavLink></li>
+            <li><NavLink to="/about" activeClassName="mobile_active">About<i className="fa fa-chevron-circle-down"></i></NavLink></li>
+            <li><NavLink to="/contact" activeClassName="mobile_active">Contact<i className="fa fa-chevron-circle-down"></i></NavLink></li>
+            <li><NavLink to="/apps" activeClassName="mobile_active">Apps<i className="fa fa-chevron-circle-down"></i></NavLink></li>
           </ul>
         </nav>
       );
